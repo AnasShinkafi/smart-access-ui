35b6215a2b07d4df8989e892865cd80cab6c17b7
unknown <anasisahskf59@gmail.com>
2025-07-31 19:50:06 +0100

    make some changes in next.config

--- src/app/dashboard/[userId]/profile/page.tsx
+++ src/app/dashboard/[userId]/profile/page.tsx
@@ -1,16 +1,29 @@
-import EditProfileForm from "../../../../components/EditProfileForm";
+// import EditProfileForm from "../../../../components/EditProfileForm";
 
-interface ProfilePageProps {
-  params: {
-    userId: string;
-  };
-}
+// interface ProfilePageProps {
+//   params: {
+//     userId: string;
+//   };
+// }
 
-export default function ProfilePage({ params }: ProfilePageProps) {
+// export default function ProfilePage({ params }: ProfilePageProps) {
+//   return (
+//     <div className="p-4 md:p-8 max-w-3xl mx-auto">
+//       <h2 className="text-2xl font-bold mb-6 text-gray-800">Edit Profile</h2>
+//       <EditProfileForm userId={params.userId} />
+//     </div>
+//   );
+// }
+
+import React from 'react'
+
+const page = () => {
   return (
-    <div className="p-4 md:p-8 max-w-3xl mx-auto">
-      <h2 className="text-2xl font-bold mb-6 text-gray-800">Edit Profile</h2>
-      <EditProfileForm userId={params.userId} />
+    <div>
+      Profile
     </div>
-  );
+  )
 }
+
+export default page
+

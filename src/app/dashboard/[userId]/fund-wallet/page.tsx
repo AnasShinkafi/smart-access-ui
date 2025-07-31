6b25c974056d89fc700cef31a62b608618802afe
unknown <anasisahskf59@gmail.com>
2025-07-31 19:45:39 +0100

    make some changes in next.config

--- src/app/dashboard/[userId]/fund-wallet/page.tsx
+++ src/app/dashboard/[userId]/fund-wallet/page.tsx
@@ -1,19 +1,31 @@
-import FundWalletForm from "../../../../components/forms/FundWalletForm";
-import WalletFundingHistory from "../../../../components/WalletFundingHistory";
+// import FundWalletForm from "../../../../components/forms/FundWalletForm";
+// import WalletFundingHistory from "../../../../components/WalletFundingHistory";
 
-interface FundWalletPageProps {
-  params: {
-    userId: string;
-  };
-}
+// interface FundWalletPageProps {
+//   params: {
+//     userId: string;
+//   };
+// }
 
-export default function FundWalletPage({ params }: FundWalletPageProps) {
+// export default function FundWalletPage({ params }: FundWalletPageProps) {
+//   return (
+//     <div className="p-4 md:p-8 max-w-2xl mx-auto">
+//       <h2 className="text-2xl font-bold mb-6 text-gray-800">Fund Wallet</h2>
+//       <FundWalletForm userId={params.userId} />
+//       <WalletFundingHistory userId={params.userId} />
+
+//     </div>
+//   );
+// }
+
+import React from 'react'
+
+const page = () => {
   return (
-    <div className="p-4 md:p-8 max-w-2xl mx-auto">
-      <h2 className="text-2xl font-bold mb-6 text-gray-800">Fund Wallet</h2>
-      <FundWalletForm userId={params.userId} />
-      <WalletFundingHistory userId={params.userId} />
-
+    <div>
+      Fund Wallet
     </div>
-  );
+  )
 }
+
+export default page
